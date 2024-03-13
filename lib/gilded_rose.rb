31cb232082b121e3d6aba54e55418c9470677e28
Danny Nguyen <dqngyen1@memphis.edu>
2024-03-13 16:36:15 -0500

    Refactor the sulfuras_tick and add empty sulfuras_tick function and 0 failures

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -13,6 +13,8 @@ class GildedRose
       return normal_item_tick
     when 'Aged Brie'
       return brie_tick
+    when 'Sulfuras, Hand of Ragnaros'
+      return sulfuras_tick
     end
     if @name != "Aged Brie" and @name != "Backstage passes to a TAFKAL80ETC concert"
       if @quality > 0
@@ -57,6 +59,9 @@ class GildedRose
         end
       end
     end
+  end
+  def sulfuras_tick
+    
   end
   def brie_tick
     @days_remaining -=1
