6c0d8d80ba4f1e234e3220cc7fcdaf0a3303a1da
Danny Nguyen <dqngyen1@memphis.edu>
2024-03-13 16:58:36 -0500

    adding more logic function handle quality and days_remaining

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -40,11 +40,21 @@ class GildedRose
     @quality += 1 if @days_remaining <= 0 && @quality < 50
   end
   def normal_item_tick
-    
-    @days_remaining -= 1
-    return if @quality == 0
+    @item = Normal.new(quality, days_remaining)
+    item.tick
+  end
+  class Normal
+    attr_reader :quality, :days_remaining
+    def initialize(quality, days_remaining)
+      @quality, @days_remaining = quality, days_remaining
+    end
+    def tick
+      
+      @days_remaining -= 1
+      return if @quality == 0
 
-    @quality -= 1
-    @quality -= 1 if @days_remaining <= 0
+      @quality -= 1
+      @quality -= 1 if @days_remaining <= 0
+    end
   end
 end
