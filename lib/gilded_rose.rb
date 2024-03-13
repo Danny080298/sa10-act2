385fadfd0b62a7b43958b1d1e3e6019506a1d742
Danny Nguyen <dqngyen1@memphis.edu>
2024-03-13 17:08:01 -0500

    Refactored sulfuras, backstage, brie into object class

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -19,30 +19,75 @@ class GildedRose
       return backstage_tick
     end
   end
-  def backstage_tick
-    @days_remaining -= 1
-    return if @quality >= 50
-    return @quality = 0 if @days_remaining < 0
 
-    @quality += 1
-    @quality += 1 if @days_remaining < 10
-    @quality += 1 if @days_remaining < 5
+
+  def backstage_tick
+    @item = Backstage.new(quality, days_remaining)
+    item.tick
+  end
+  class Backstage
+    attr_reader :quality, :days_remaining
+    def initialize(quality, days_remaining)
+      @quality, @days_remaining = quality, days_remaining
+    end
+    def tick 
+      @days_remaining -= 1
+      return if @quality >= 50
+      return @quality = 0 if @days_remaining < 0
+
+      @quality += 1
+      @quality += 1 if @days_remaining < 10
+      @quality += 1 if @days_remaining < 5
+    end
   end
   
   def sulfuras_tick
-    
+    @item = Sulfuras.new(quality, days_remaining)
+    item.tick
   end
-  def brie_tick
-    @days_remaining -=1
-    return if @quality >= 50
+  
+  class Sulfuras
+    attr_reader :quality, :days_remaining
+    def initialize(quality, days_remaining)
+      @quality, @days_remaining = quality, days_remaining
+    end
+    def tick
+    end
+  end
 
-    @quality += 1
-    @quality += 1 if @days_remaining <= 0 && @quality < 50
+
+  def brie_tick
+    @item = Brie.new(quality, days_remaining)
+    item.tick
   end
+  class Brie
+    attr_reader :quality, :days_remaining
+    def initialize (quality, days_remaining)
+      @quality, @days_remaining = quality, days_remaining
+    end
+    def tick
+      @days_remaining -=1
+      return if @quality >= 50
+
+      @quality += 1
+      @quality += 1 if @days_remaining <= 0 && @quality < 50
+    end
+  end
+
   def normal_item_tick
     @item = Normal.new(quality, days_remaining)
     item.tick
   end
+
+  def quality
+    return item.quality if item
+  end
+
+  def days_remaining
+    return item.days_remaining if item
+    @days_remaining
+  end
+  
   class Normal
     attr_reader :quality, :days_remaining
     def initialize(quality, days_remaining)
